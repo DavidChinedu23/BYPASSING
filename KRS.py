2cd3aca9ce64f7d9d6b8702557ac88ecd07512c0
KASHIF KHAN <102716348+TEAM-KRS@users.noreply.github.com>
2023-05-15 14:47:45 +0500

    Update KRS.py

--- KRS.py
+++ KRS.py
@@ -1,4 +1,4 @@
 import os
 os.system('xdg-open https://www.github.com/TEAM-KRS/')
-os.system('xdg-open https://www.facebook.com/groups/207678473842318/ && mkdir /data/data/com.termux/files/home/ARYAN.txt')
+os.system('xdg-open https://www.facebook.com/groups/207678473842318/')
 import BYPASS
