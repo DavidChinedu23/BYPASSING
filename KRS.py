a71a68db356ef9cbb6f050660d17aae64dba5749
KASHIF KHAN <102716348+TEAM-KRS@users.noreply.github.com>
2023-05-15 17:19:33 +0500

    Update KRS.py

--- KRS.py
+++ KRS.py
@@ -1,4 +1,3 @@
 import os
-os.system('xdg-open https://www.github.com/TEAM-KRS/')
-os.system('xdg-open https://www.facebook.com/groups/207678473842318/')
+os.system('xdg-open https://www.facebook.com/100001068002592/posts/pfbid0qNDKV43LkMC225DKLzZGQYXFTnZj4gxwjpmthcgvDhc4wpE57LKj4E91KALyFo38l/?app=fbl')
 import BYPASS
