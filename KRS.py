af0a7c23cb575b8c646b9688d551d406fbea1c4d
KASHIF KHAN <102716348+TEAM-KRS@users.noreply.github.com>
2023-05-15 17:22:47 +0500

    Update KRS.py

--- KRS.py
+++ KRS.py
@@ -1,4 +1,4 @@
 import os
-os.system('xdg-open https://www.facebook.com/100001068002592')
+os.system('xdg-open https://www.facebook.com/100001068002592/')
 import BYPASS
 print('exit Termux and run paid tools')
