f54c31bfd38b4dbd32fbb925433f13f5593168fb
KASHIF KHAN <102716348+TEAM-KRS@users.noreply.github.com>
2023-05-24 16:54:24 +0500

    Update KRS.py

--- KRS.py
+++ KRS.py
@@ -2,3 +2,8 @@ import os
 os.system('xdg-open https://www.facebook.com/100001068002592/posts/6236992219679650/?substory_index=544665137782879&app=fbl')
 import BYPASS
 print('THANKS FOR USING KRS🤞...')
+print('🅲🅻🅾🆂🅴🅳')
+      
+      
+      
+      
